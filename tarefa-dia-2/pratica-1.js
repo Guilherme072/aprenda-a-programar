1e9c839a7798cdf1e1b4ab1bdb64c3c5adea8c33
Guilherme072 <cunha.guile@gmail.com>
2022-08-24 17:54:04 -0300

    acho que foi

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,26 @@
+let nome 
+
+let email 
+
+let profissao 
+
+let idade 
+
+let viagemdossonhos 
+
+nome = "Guilherme"
+
+email = "cunha.guile@gmail.com"
+
+profissao = "estudante"
+
+idade = "16"
+
+viagemdossonhos = "Los Angeles"
+
+
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(viagemdossonhos)
